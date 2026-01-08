de24eeddba731d594b1964a5bf8970e4c2049884
its_emora <elliotmoran10@gmail.com>
2026-01-08 21:32:34 +0000

    Added very basic tilemap and gravity, no collisions

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ GREEN = (0,255,0)
 BLUE = (0,0,255)
 # Strings
 GAME_NAME = "Squirrel Fall"     # The game title.
+# Integers
+TILE_SIZE = 64
+SCREEN_WIDTH,SCREEN_HEIGHT = 1920,1080
 # Booleans
 running = True      # Whether the main game loop should run or not.
 # Miscellaneous
@@ -28,8 +31,8 @@ clock = pygame.time.Clock()     # Declaring the clock.
 map = map.map       # The tilemap.
 # Sprite groups
 player_group = pygame.sprite.Group()
-
-
+main_tile_group = pygame.sprite.Group()
+dirt_tile_group = pygame.sprite.Group()
 
 
 # ---- SETTING UP THE GAME WINDOW ---- #
@@ -63,32 +66,72 @@ class PLAYER(pygame.sprite.Sprite):
     # Updating the player.
     def update(self,dt):
         self.acceleration = vector(0,0)     # Reseting the acceleration to fix bouncing bug.
-        self.multiplier = 0
+        self.multiplier = 0     # Multiplier for velocity. This is so that if you press both A and D, you do not move.
 
         if keys[pygame.K_d]:
-            self.multiplier += 0.5 
-            self.image = load("assets/images/player/player_facing_right.png")
+            self.multiplier += 1        # Positive multiplier to increase x value.
+            self.image = load("assets/images/player/player_facing_right.png")       # Loading the facing right image.
         if keys[pygame.K_a]:
-            self.multiplier -= 0.5
-            self.image = load("assets/images/player/player_facing_left.png")
+            self.multiplier -= 1        # Negative multiplier to decrease y value.
+            self.image = load("assets/images/player/player_facing_left.png")        # Loading the facing left image.
 
+        # Kinematic movement equations.
         self.acceleration.x = self.HORIZONTAL_ACCELERATION * self.multiplier
 
         self.acceleration.x -= self.velocity.x * self.FRICTION_COEFFICIENT
         self.velocity += self.acceleration
         self.position += self.velocity + self.acceleration / 2 * dt
 
+        # Checking if the squirrel is off the screen.
         if self.position.x > 2000:
             self.position.x = -100
         if self.position.x < -100:
             self.position.x = 2000
 
-        self.rect.center = self.position
+        self.rect.center = self.position        # Setting the new position
+
+# ---- TILE CLASS
+class TILE(pygame.sprite.Sprite):
+    def __init__(self,x,y,tile_int,main_group,sub_group):
+        super().__init__()
+
+        if tile_int == 1:
+            self.image = load("assets/images/tiles/tile_dirt_texture.png")
+            sub_group.add(self)
+        
+        main_group.add(self)
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+
+        self.position = vector(x,y)
+
+        self.velocity = vector(0,0)
+        self.acceleration = vector(0,0)
+        self.GRAVITY = 0.1
+
+    
+    def update(self):
+        self.acceleration = vector(0,self.GRAVITY)
+
+        self.velocity -= self.acceleration
+        self.position += self.velocity + self.acceleration / 2
+
+        self.rect.topleft = self.position
+        
 
 
 # ---- MAKING THE PLAYER ---- #
 player = PLAYER(960,540)
 player_group.add(player)
+
+
+# ---- MAKING THE MAP ---- #
+for row in range(len(map)):
+    for col in range(len(map[row])):
+        if map[row][col] == 1:
+            tile = TILE(col*TILE_SIZE,row*TILE_SIZE,map[row][col],main_tile_group,dirt_tile_group)
+
 
 
 # ---- GAME LOOP ---- #
@@ -108,6 +151,9 @@ while running:
 
     player_group.update(delta_time)
     player_group.draw(root)
+
+    main_tile_group.update()
+    main_tile_group.draw(root)
 
     pygame.display.flip()       # Flipping the display.
 
